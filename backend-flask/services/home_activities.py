d44e31e0c1a3d7af721cf1ea4b9f703d591c388f
SDJ <robineurus@gmail.com>
2023-03-04 14:22:41 +0000

    add spans and attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,7 +6,7 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
-      span = tracer.start_as_current_span()
+      span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       results = [{
